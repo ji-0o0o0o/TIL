34fff744fac271d447769c744df380b77e576f6a
ji-0o0o0o <tlswldud28@naver.com>
2022-12-10 12:24:22 +0900

    221210

--- java/복습/객체지향2/디폴트메서드와_static메서드.java
+++ java/복습/객체지향2/디폴트메서드와_static메서드.java
@@ -4,7 +4,7 @@ public class 디폴트메서드와_static메서드 {
     public static void main(String[] args) {
         Child3 c = new Child3();
         c.method1(); //child3
-        c.method2(); //Parent3
+        c.method2(); //Parent3에서 상속 받은 method 출력
         MyInterface.staticMethod();//static메서드에서는 static 메서드 가져올 수 있음
         MyInterface2.staticMethod();
     }
